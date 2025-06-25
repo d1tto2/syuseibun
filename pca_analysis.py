a0946b1740ce78b8b7b8014e2df88dcaae48126d
Hayato Araki <cnt14003@ict.nitech.ac.jp>
2025-06-26 07:25:32 +0900

    feat: 相関行列を算出し固有値分解を実行

--- pca_analysis.py
+++ pca_analysis.py
@@ -15,3 +15,23 @@ std = D.std(axis = 0,ddof = 1)
 SD = (D - mean) / std
 
 print("データの標準化が完了しました。")
+
+# --- ここから追記 ---
+# 相関行列の計算 (標準化データからなので共分散行列が相関行列になる)
+R = np.cov(SD, rowvar=False)
+print("R = ", R)
+
+# 固有値と固有ベクトルを計算
+w, v = np.linalg.eig(R)
+
+print("相関行列の計算と固有値分解が完了しました。")
+
+# --- ここから追記 ---
+# 相関行列の計算 (標準化データからなので共分散行列が相関行列になる)
+R = np.cov(SD, rowvar=False)
+print("R = ", R)
+
+# 固有値と固有ベクトルを計算
+w, v = np.linalg.eig(R)
+
+print("相関行列の計算と固有値分解が完了しました。")
